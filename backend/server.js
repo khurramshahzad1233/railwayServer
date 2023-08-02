f1d780490d58bd8fd9cb36b8feae1e2612da4aa6
khurramshahzad1233 <ambreenkhurram004@gmail.com>
2023-08-02 10:56:45 +0500

    dkf

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ if(process.env.NODE_ENV!=="PRODUCTION"){
     dotenv.config({path:"backend/config.env"})
 };
 
+app.get('/', (req, res) => {
+    res.send('Hello, Railway!');
+  });
+
 const port=process.env.PORT || 3000
 
 app.listen(port,"0.0.0.0",()=>{
