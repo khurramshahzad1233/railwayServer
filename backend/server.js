6eef41adf77b54b3a79ed969719d689377f6e864
khurramshahzad1233 <ambreenkhurram004@gmail.com>
2023-08-02 10:20:59 +0500

    cha

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 import app from "./app.js"
 import dotenv from "dotenv"
 
-// if(process.env.NODE_ENV!=="PRODUCTION"){
+if(process.env.NODE_ENV!=="PRODUCTION"){
     dotenv.config({path:"backend/config.env"})
-// };
+};
 
-const port=1234 || process.env.port
+const port=process.env.port || 1234
 
-app.listen(port,()=>{
+app.listen(port,"0,0,0,0",()=>{
     console.log(`server is running on port ${port}`)
 })
